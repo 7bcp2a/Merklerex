f6302d520c8f4d72dd9709d9ddea23bebabd6296
7bcp2a <nilphadrell@gmail.com>
2022-04-18 13:08:53 +0200

    created vector of objects

--- main.cpp
+++ main.cpp
@@ -160,13 +160,28 @@ int main()
     //     processUserOption(userOption);
     // }
 
+    std::vector<OrderBookEntry> orders;
+
+
     OrderBookEntry order1{  1000,
                             0.02, 
                             "2020/03/17 17:01:24.884492", 
                             "BTC/USDT",
                             OrderBookType::bid};
 
-    std::cout << "The price is " << order1.price << std::endl;
+    orders.push_back(   OrderBookEntry{  1000,
+                            0.02, 
+                            "2020/03/17 17:01:24.884492", 
+                            "BTC/USDT",
+                            OrderBookType::bid});
+    orders.push_back(   OrderBookEntry{  2000,
+                            0.02, 
+                            "2020/03/17 17:01:24.884492", 
+                            "BTC/USDT",
+                            OrderBookType::bid});
+
+
+    std::cout << "The price is " << orders[1].price << std::endl;
 
     return 0;
 }
