10f724478aff067696c7def1a1923916cace10f3
7bcp2a <nilphadrell@gmail.com>
2022-05-01 14:15:23 +0200

    created separate files OrderBookEntry (.h and .cpp)

--- main.cpp
+++ main.cpp
@@ -2,35 +2,6 @@
 #include <string>
 #include <vector>
 
- enum class OrderBookType{bid, ask};
-
- class OrderBookEntry
- {
-     public:
-
-        OrderBookEntry( double _price,
-                        double _amount,
-                        std::string _timestamp,
-                        std::string _product,
-                        OrderBookType _orderType)
-        :   price(_price), 
-            amount(_amount), 
-            timestamp(_timestamp),
-            product(_product)
-
-        {
-      
-        }
-
-        double price;
-        double amount;
-        std::string timestamp;
-        std::string product;
-        OrderBookType orderType;
- };
-
-
-
 void printMenu()
 {
     // 1 print help
