79333aa797ab454e2e307bd7d31d4d65567505a6
7bcp2a <nilphadrell@gmail.com>
2022-06-08 22:24:20 +0200

    Implement print and remove functions

--- main.cpp
+++ main.cpp
@@ -10,10 +10,12 @@
  {
    //  MerkelMain app{};
    //  app.init();
-    Wallet wallet;
-    wallet.insertCurrency("BTC", 10);
-    std::cout << "Wallet has BTC " << wallet.containsCurrency("USDT", 10) << std::endl;
-    std::cout << wallet.toString() << std::endl;
+   Wallet wallet;
+   wallet.insertCurrency("BTC", 10);
+   wallet.insertCurrency("USDT", 10000);
+   std::cout << wallet.toString() << std::endl;
+   wallet.removeCurrency("USDT", 1000);
+   std::cout << wallet.toString() << std::endl;
  }
 
    
